d4ccb769000afd483ba9554c06398ce0d9672dd5
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-10-12 22:15:08 +0300

    Updates

---
+++ precache-manifest.210c23cf29047254a9f2cae872604020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67dd7e640cf573ebaf60dcbf8552c32d",
+    "url": "/Team_project/index.html"
+  },
+  {
+    "revision": "d83bc68a4a4e5a25e573",
+    "url": "/Team_project/static/css/main.f7faeac2.chunk.css"
+  },
+  {
+    "revision": "e52e356308b9428cdd2d",
+    "url": "/Team_project/static/js/2.b18a52ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Team_project/static/js/2.b18a52ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83bc68a4a4e5a25e573",
+    "url": "/Team_project/static/js/main.348aa753.chunk.js"
+  },
+  {
+    "revision": "6e9f4b3228b232845ce1",
+    "url": "/Team_project/static/js/runtime-main.5af60c6a.js"
+  }
+]);
