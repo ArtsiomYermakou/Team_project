ec43a33143f4e2ba5844257f84f79e59bb51473a
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-10-18 20:34:39 +0300

    add css in forgotPassword

--- src/n1_main/m1-ui/component/ForgotPassword/ForgotPassword.tsx
+++ src/n1_main/m1-ui/component/ForgotPassword/ForgotPassword.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import InputMaterial from "../../common/Input";
+import style from "../ForgotPassword/ForgotPassword.module.css"
 
 const ForgotPassword = () => {
     return <div>
+        <h1 className={style.title}>Forgot Password</h1>
         <InputMaterial placeholder={"password"} label={"password"} />
     </div>
 }
